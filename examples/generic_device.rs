00d068cc5c17a4a84ae4e5b1bf02c83c74d28448
KX <kx@imkx.dev>
2024-09-30 12:54:08 +0200

    fix: generic device example

--- examples/generic_device.rs
+++ examples/generic_device.rs
@@ -1,8 +1,12 @@
 use regex::Regex;
-use rustmiko::devices::generic::connection::{Connection, TelnetConnection};
+use rustmiko::devices::generic::connection::{Connection, ConnectionOptions, TelnetConnection};
 
 fn main() {
-    let mut device = TelnetConnection::connect("192.168.1.101:23", Some("admin"), Some("password")).unwrap();
+    let mut device = TelnetConnection::connect("192.168.1.101:23", &ConnectionOptions {
+        username: Some("admin"),
+        password: Some("password"),
+        timeout: None
+    }).unwrap();
     let prompt_end = Regex::new(r"[#>]").unwrap();
 
     device.execute_raw("show running configuration", &prompt_end).unwrap();
